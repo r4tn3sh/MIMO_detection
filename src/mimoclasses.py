a4f351165f069e598cee392d6e5eba8c4b5ffd31
ratnesh <ratnesh.kumbhkar@gmail.com>
2021-01-25 22:30:15 -0800

    Created a generic channel parameter based model
    * Added a new file for future performance measurements.

--- src/mimoclasses.py
+++ src/mimoclasses.py
@@ -1,5 +1,6 @@
 import math
 
+from dataclasses import dataclass
 from enum import Enum
 from enum import IntEnum
 
@@ -9,6 +10,16 @@ class Equalizer(Enum):
 
 class Channel(IntEnum):
     NONE = 1
-    RAND_UNIT = 2
+    RAND_UNIT = 2 # Completely stochastic
     RAND_UNIT_GOOD = 3
     RAND_UNIT_BAD = 4
+    FSPL = 5 # Free space path loss
+    RAYLEIGH = 6
+    RICIAN = 7
+
+@dataclass
+class ChParam:
+    samprate: int
+    delay: float
+    doppler: float
+    seed: int
