f08e3c0de2429434c08fb46e016f92de0c38b7a1
ratnesh <ratnesh.kumbhkar@gmail.com>
2021-01-28 17:58:29 -0800

    Fixed EVM bug and added plot legends

--- src/measurefunc.py
+++ src/measurefunc.py
@@ -5,10 +5,15 @@ def getEVM (sampIn, sampOut, mod):
     if sampIn.shape == sampOut.shape:
         err = (sampIn-sampOut)
         evmlin = np.mean(np.square(np.absolute(err)))
-        evm = 10*np.log10(evmlin*normFactor(mod))
-        print(evmlin,evm, normFactor(mod))
-        return evm
+        # Since the constellation already has normalized power, no need to
+        # further normalize for EVM calculations.
+        evm = 10*np.log10(evmlin)
+        # There is maximum value of EVM that can be resolved by the signal
+        # analyzers in real test environment.
+        maxevm = -10*np.log10(normFactor(mod))
+        return min(evm, maxevm)
     else:
+        print(sampIn.shape, sampOut.shape)
         raise ValueError('size of both parameters should be same')
 
 
@@ -17,4 +22,5 @@ def getSER (sampIn, sampOut):
         nofsamp_err = ((sampIn-sampOut)>0).sum(dtype='float')
         return nofsamp_err
     else:
+        print(sampIn.shape, sampOut.shape)
         raise ValueError('size of both parameters should be same')
--- src/plotfunc.py
+++ src/plotfunc.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+from numpy import array
 
 # ---------- PLOTS ------------
 def plotConstell(y):
     """
     Plots the constellation of given samples.
     """
-    yr = [a.real for a in y]
-    yi = [a.imag for a in y]
-    p = plt.figure()
-    plt.scatter(yr, yi,s=3)
+    yr = y.real#[a.real for a in y]
+    yi = y.imag#[a.imag for a in y]
+    nrow, ncol = y.shape
+    #p = plt.figure()
+    p, ax = plt.subplots()
+    for idx in range(nrow):
+        plt.scatter(array(yr[idx]), array(yi[idx]), s=3, label='path '+str(idx+1))
+    ax.legend()
     return p
